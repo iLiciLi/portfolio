9771d2fafc8bc42783c124d2c11315fd9390ea0b
Pavle Ilic <pavleilic290@gmail.com>
2023-11-26 13:13:08 +0100

    Dodat je responsive za sajt

--- app.js
+++ app.js
@@ -8,22 +8,6 @@ document.querySelectorAll('a[href^="."]').forEach(anchor => {
     });
 });
 
-function addHoverEffect(elementId) {
-    const element = document.getElementById(elementId);
 
-    element.addEventListener('mouseenter', function() {
-        element.style.opacity = 1;
-    });
-
-    element.addEventListener('mouseleave', function() {
-        element.style.opacity = 0;
-    });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Call the function for the specific elements
-    addHoverEffect('obrazovanje');
-    addHoverEffect('profilP');
-});
 
 
