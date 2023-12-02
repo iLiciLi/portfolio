77bfc2e8fec1f83c4e55f677b53a65e6a864d02f
Pavle Ilic <pavleilic290@gmail.com>
2023-12-02 18:20:46 +0100

    Dodata putanja za slike u vestine.js fajlu

--- javaData/vestine.js
+++ javaData/vestine.js
@@ -1,12 +1,12 @@
 vestineData = [
 
-    {"naziv":"html","ocena":7,"godina_iskustva":2},
-    {"naziv":"css","ocena":5,"godina_iskustva":2},
-    {"naziv":"javascript","ocena":3,"godina_iskustva":0},
-    {"naziv":"c#","ocena":8,"godina_iskustva":1},
-    {"naziv":"python","ocena":7,"godina_iskustva":3},
-    {"naziv":"godot game engine","ocena":8,"godina_iskustva":1.5},
-    {"naziv":"win forms","ocena":5,"godina_iskustva":1},
-    {"naziv":"c++","ocena":3,"godina_iskustva":2},
+    {"slika":"javaData/javaSlike/htmlS.png","naziv":"html","ocena":7,"godina_iskustva":2},
+    {"slika":"javaData/javaSlike/cssS.png","naziv":"css","ocena":5,"godina_iskustva":2},
+    {"slika":"javaData/javaSlike/jsS.png","naziv":"javascript","ocena":3,"godina_iskustva":0},
+    {"slika":"javaData/javaSlike/csS.png","naziv":"c#","ocena":8,"godina_iskustva":1},
+    {"slika":"javaData/javaSlike/pyS.png","naziv":"python","ocena":7,"godina_iskustva":3},
+    {"slika":"javaData/javaSlike/gdS.png","naziv":"godot game engine","ocena":8,"godina_iskustva":1.5},
+    {"slika":"javaData/javaSlike/wfS.png","naziv":"win forms","ocena":5,"godina_iskustva":1},
+    {"slika":"javaData/javaSlike/cppS.png","naziv":"c++","ocena":3,"godina_iskustva":2},
     
 ]
