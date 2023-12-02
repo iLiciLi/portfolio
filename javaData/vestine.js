b3b8a2187a0cc18ab844e93fc45984e373a3ad8a
Pavle Ilic <pavleilic290@gmail.com>
2023-12-02 18:51:28 +0100

    Dodata klasa za svaku vestinu

--- javaData/vestine.js
+++ javaData/vestine.js
@@ -1,12 +1,12 @@
 vestineData = [
 
-    {"slika":"javaData/javaSlike/htmlS.png","naziv":"html","ocena":7,"godina_iskustva":2},
-    {"slika":"javaData/javaSlike/cssS.png","naziv":"css","ocena":5,"godina_iskustva":2},
-    {"slika":"javaData/javaSlike/jsS.png","naziv":"javascript","ocena":3,"godina_iskustva":0},
-    {"slika":"javaData/javaSlike/csS.png","naziv":"c#","ocena":8,"godina_iskustva":1},
-    {"slika":"javaData/javaSlike/pyS.png","naziv":"python","ocena":7,"godina_iskustva":3},
-    {"slika":"javaData/javaSlike/gdS.png","naziv":"godot game engine","ocena":8,"godina_iskustva":1.5},
-    {"slika":"javaData/javaSlike/wfS.png","naziv":"win forms","ocena":5,"godina_iskustva":1},
-    {"slika":"javaData/javaSlike/cppS.png","naziv":"c++","ocena":3,"godina_iskustva":2},
+    {"class":"htmlK","slika":"javaData/javaSlike/htmlS.png","naziv":"html","ocena":7,"godina_iskustva":2},
+    {"class":"cssK","slika":"javaData/javaSlike/cssS.png","naziv":"css","ocena":5,"godina_iskustva":2},
+    {"class":"jsK","slika":"javaData/javaSlike/jsS.png","naziv":"javascript","ocena":3,"godina_iskustva":0},
+    {"class":"csK","slika":"javaData/javaSlike/csS.png","naziv":"c#","ocena":8,"godina_iskustva":1},
+    {"class":"pyK","slika":"javaData/javaSlike/pyS.png","naziv":"python","ocena":7,"godina_iskustva":3},
+    {"class":"gdK","slika":"javaData/javaSlike/gdS.png","naziv":"godot game engine","ocena":8,"godina_iskustva":1.5},
+    {"class":"wfK","slika":"javaData/javaSlike/wfS.png","naziv":"win forms","ocena":5,"godina_iskustva":1},
+    {"class":"cppK","slika":"javaData/javaSlike/cppS.png","naziv":"c++","ocena":3,"godina_iskustva":2},
     
 ]
