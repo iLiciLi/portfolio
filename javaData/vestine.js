ff2bc895cc4c17b429c05e14e33dcdb817819013
Pavle Ilic <pavleilic290@gmail.com>
2023-12-02 18:04:31 +0100

    Podaci su dodati u vestine.js

---
+++ javaData/vestine.js
@@ -0,0 +1,12 @@
+vestineData = [
+
+    {"naziv":"html","ocena":7,"godina_iskustva":2},
+    {"naziv":"css","ocena":5,"godina_iskustva":2},
+    {"naziv":"javascript","ocena":3,"godina_iskustva":0},
+    {"naziv":"c#","ocena":8,"godina_iskustva":1},
+    {"naziv":"python","ocena":7,"godina_iskustva":3},
+    {"naziv":"godot game engine","ocena":8,"godina_iskustva":1.5},
+    {"naziv":"win forms","ocena":5,"godina_iskustva":1},
+    {"naziv":"c++","ocena":3,"godina_iskustva":2},
+    
+]
